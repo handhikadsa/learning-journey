903c3bac97102ebc86cb72464f9159086b6fbd9e
Dhika <59341485+handhikadsa@users.noreply.github.com>
2024-03-08 17:35:44 +0700

    update

--- my-app-tailwind/src/app/components/todo/EditTodoList.js
+++ my-app-tailwind/src/app/components/todo/EditTodoList.js
@@ -6,22 +6,23 @@ const EditTodoList = ({ openModal, setOpenModal  }) => {
 
     return (
         <Modal show={openModal} onClose={() => setOpenModal(false)}>
-            <Modal.Header>Terms of Service</Modal.Header>
+            <Modal.Header>Edit Todo</Modal.Header>
             <Modal.Body>
-            <div className="space-y-6">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-                companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
-                to ensure a common set of data rights in the European Union. It requires organizations to notify users as
-                soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-            </div>
+                <div className="space-y-6">
+                    <form className="">
+                        <div className="mb-5">
+                            <label for="small-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                            <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" />
+                        </div>
+                        <div>
+                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Todo</label>
+                            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a word..."></textarea>
+                        </div>
+                    </form> 
+                </div>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={() => setOpenModal(false)}>I accept</Button>
+                <Button type='submit'>I accept</Button>
                 <Button color="gray" onClick={() => setOpenModal(false)}>
                     Decline
                 </Button>
